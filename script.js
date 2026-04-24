3b4d6419e8dc7cd793e08d0441ffa9de94588733
Vic-Soso <158002815+Vic-Soso@users.noreply.github.com>
2026-04-24 08:55:00 +0100

    add project files

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 const bookList = document.getElementById("bookList");
-const nav = document.getElementById("nav");
-
-
-
-async function fetchBooks(query="harry potter"){
-    const response = await fetch(`https://openlibrary.org/search.json?q=${query}`);
-    const data = await response.json();
-
-    displayBooks(data.docs.slice(0,12));
+function toggleMenu() {
+    document.querySelector(".navbar").classList.toggle("active");
 }
 
-function displayBooks(books){
+function getBooks(query) {
+    if (!query) query = "harry potter";
+
+    fetch("https://openlibrary.org/search.json?q=" + query)
+        .then(res => res.json())
+        .then(data => {
+            showBooks(data.docs.slice(0, 15));
+            // console.log("data", data);
+            // console.log("books", data.docs);
+        });
+        
+}
+
+function showBooks(books) {
     bookList.innerHTML = "";
+    // console.log(books);
 
-    books.forEach(book => {
-        let image = book.cover_i
-        ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-        : "https://via.placeholder.com/200x250";
+    books.forEach(function(book) {
+        let image;
 
-        bookList.innerHTML += `
-        <div class="book">
-            <img src="${image}">
-            <h3>${book.title}</h3>
-            <p>${book.author_name ? book.author_name[0] : "Unknown"}</p>
-        </div>
-        `;
+        if (book.cover_i) {
+            image = "https://covers.openlibrary.org/b/id/" + book.cover_i + "-M.jpg";
+        } else {
+            image = "nil";
+        }
+
+        let author = book.author_name ? book.author_name[0] : "Unknown";
+
+        bookList.innerHTML +=
+            "<div class='book'>" +
+                "<img src='" + image + "'>" +
+                "<h3>" + book.title + "</h3>" +
+                "<p>" + author + "</p>" +
+            "</div>";
     });
+    
 }
 
-function searchBooks(){
+function searchBooks() {
     let query = document.getElementById("searchInput").value;
-    fetchBooks(query);
+    getBooks(query);
 }
 
-fetchBooks();
+getBooks();
